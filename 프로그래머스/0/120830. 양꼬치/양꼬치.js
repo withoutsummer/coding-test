de8fc64471eaa9e573838228c379735af0235aa8
Kosieun <106417059+withoutsummer@users.noreply.github.com>
2025-08-07 18:48:01 +0900

    [level 0] Title: 양꼬치, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.js
@@ -0,0 +1,11 @@
+function solution(n, k) {
+    var answer = 0;
+    
+    if(n >= 10){
+        let service = Math.floor(n/10)
+        return answer = (n * 12000) + ((k-service)*2000);
+    }
+    else{
+        return answer = (n * 12000) + (k*2000);
+    }
+}
