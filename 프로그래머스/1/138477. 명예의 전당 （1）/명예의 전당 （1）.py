00454e205a0ce3f5c1d8189b14c2279430f14973
Kosieun <106417059+withoutsummer@users.noreply.github.com>
2026-01-14 00:58:53 +0900

    [level 1] Title: 명예의 전당 (1), Time: 1.03 ms, Memory: 9.35 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -0,0 +1,27 @@
+def solution(k, score):
+    answer = []
+    arr = []
+    day = 1
+    
+    for i in score:
+        if day <= k:
+            arr.append(i)
+            answer.append(min(arr))
+            day += 1
+        else:
+            arr.append(i)
+            arr.sort(reverse = True)
+            arr.pop()
+            answer.append(arr[-1])
+            day += 1
+            
+    return answer
+            
+        
+
+        
+        
+        
+        
+    
+    return answer
