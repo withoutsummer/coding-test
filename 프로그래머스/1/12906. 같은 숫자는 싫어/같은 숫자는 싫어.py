9f6e39b84452cb6b51de94c32443531df9220823
Kosieun <106417059+withoutsummer@users.noreply.github.com>
2026-01-22 12:44:59 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 83.10 ms, Memory: 27.1 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,13 +1,13 @@
 def solution(arr):
-    answer = []
-    ex = -1
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    for i,n in enumerate(arr):
-        if i != 0:
-            ex = arr[i-1]
+    st = []
+    
+    for i,cur in enumerate(arr):
+        # print(f"i:{i}, cur:{cur}일 때")
         
-        if ex != n:
-            answer.append(n)
+        # print(f"st: {st}")
+        if not st or cur != st[-1]:
+            # print(f"st에 {cur} 추가")
+            st.append(cur)
             
-        
-    return answer
+    # print("st:",st)
+    return st
