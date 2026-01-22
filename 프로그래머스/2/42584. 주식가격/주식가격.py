a729f2a23111061179e378c95c0c0f65f930586c
Kosieun <106417059+withoutsummer@users.noreply.github.com>
2026-01-22 12:13:35 +0900

    [level 2] Title: 주식가격, Time: 24.40 ms, Memory: 19 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,20 +1,25 @@
-from collections import deque
-
 def solution(prices):
-    time_arr= []
-    q_prices = deque(prices)
+    n = len(prices)
+    idx_st= [] # 아직 가격이 떨어진 적 없는 인덱스
+    answer =[0]*n # j시점 가격이 처음으로 떨어지는 시점까지 걸린 시간
     
-    for _ in range(len(q_prices)):
-        cur = q_prices.popleft()
-        count = 0
+    for i,cur in enumerate(prices):
         
-        for i in q_prices:
-            if cur <= i:
-                count += 1
-            else: #현재 가격보다 클 경우, 카운트 하고 종료
-                count += 1
-                break
+        # print("i:",i,"cur:",cur)
+        # print("idx_st:",idx_st)
+        # 가격이 떨어지는 순간 / 이전 인덱스 값 현재 값보다 클 수 있으므로 while문으로 반복
+        while idx_st and prices[idx_st[-1]] > cur:
+            j = idx_st.pop() # 스택 맨 위 인덱스
+            # print("j:", j)
+            answer[j] = i - j
+            # print("idx_st:",idx_st,"answer:",answer)
+        # 현재 인덱스는 떨어지지 않은 스택이므로 저장    
+        idx_st.append(i)
         
-        time_arr.append(count)
+    # 최종 떨어지지 않은 인덱스를 배열 길이에서 빼서 기간 계산
+    while idx_st:
+        j = idx_st.pop()
+        answer[j] = (n-1)-j
+        
     
-    return time_arr
+    return answer
