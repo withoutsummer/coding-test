8b50e53cfc1aed775fe5a10ece1a0fc906f7f6fe
Kosieun <106417059+withoutsummer@users.noreply.github.com>
2026-01-24 09:53:01 +0900

    [level 2] Title: 구명보트, Time: 9.70 ms, Memory: 9.51 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,16 +1,13 @@
 def solution(people, limit):
-    boats = 0
-    left = 0
-    right = len(people) - 1
     people.sort()
-    
+    left, right = 0, len(people)-1
+    boats = 0
     
     while left <= right:
         if people[left] + people[right] <= limit:
             left += 1
+
         right -= 1
         boats += 1
-    
-    
-    
+       
     return boats
