f631fe1b59be2c6ef151d56b0829172acfa52b14
Kosieun <106417059+withoutsummer@users.noreply.github.com>
2026-01-27 18:28:05 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.py
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -1,18 +1,19 @@
 def solution(a, b):
-    # 인덱스 1~12까지만 사용
-    days_of_month_list = [0,31,29,31,30,31,30,31,31,30,31,30,31]
-
-    # 인덱스 1~7 사용
-    day_of_week_list = ["","SUN","MON","TUE","WED","THU","FRI","SAT"]
-    first_idx = 6
-
-    prev_month_days = 0
-    totals = 0
+    week = ["","SUN","MON","TUE","WED","THU","FRI","SAT"]
+    last_day= [0,31,29,31,30,31,30,31,31,30,31,30,31]
     
-    for i in range(1,a):
-        prev_month_days += days_of_month_list[i]
+    ex_days = 0
+    for d in range(1,a):
+        ex_days += last_day[d]
+        
+    print(ex_days)
+    total_days = ex_days + b
+    w = total_days % 7 
+    print(w)
     
-    totals = prev_month_days + b
-    cur_idx = totals % 7 + (first_idx - 1) if totals % 7 + (first_idx - 1) <= 7 else (totals % 7 + (first_idx - 1)) % 7
+    if w + 5 > 7:
+        return week[(w + 5) % 7]
+    else:
+        return week[w + 5]
     
-    return day_of_week_list[cur_idx]
+    
--- 프로그래머스/1/12901. 2016년/README.md
+++ 프로그래머스/1/12901. 2016년/README.md
@@ -1,10 +1,10 @@
 # [level 1] 2016년 - 12901 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901?utm_source=chatgpt.com) 
 
 ### 성능 요약
 
-메모리: 9.23 MB, 시간: 0.01 ms
+메모리: 9.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 14일 16:10:10
+2026년 01월 27일 18:28:01
 
 ### 문제 설명
 
