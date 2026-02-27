31202e8de19de1811be3fdfd15839e81aa2b69df
Kosieun <106417059+withoutsummer@users.noreply.github.com>
2026-02-27 13:40:03 +0900

    [level 2] Title: 올바른 괄호, Time: 5.90 ms, Memory: 9.47 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,19 +1,35 @@
+
+from collections import deque
+
 def solution(s):
-    answer = False
-    st = []
-    if s[0] == ")" or len(s) % 2 != 0:
+    q = deque()
+    
+    for i in s:
+        if i == '(':
+            q.append(i)
+        else:
+            if not q:
+                return False
+            else:
+                q.popleft()
+            
+    if not q:
+        return True
+    else:
         return False
 
-    if s.count("(") > len(s) // 2:
-        return False
     
-    for ch in s:
-        if ch == "(":
-            st.append(ch)
-        
-        else:
-            if not st:
-                return False
-            st.pop()
-        
-    return True
+
+
+
+
+
+
+    
+
+# 1. '('이 나올 경우 큐에 삽입
+
+# 2. ')'이 나올 경우
+#   2-1. 큐가 빈 경우, 바로 false 리턴 
+#   2-2. 큐 존재 할 경우, popleft()
+# 3. 모두 돌고 아직 큐에 값이 존재할 경우 false 리턴
